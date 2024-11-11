e22c081e8d9964dd0c246eeb79e9d15b98edd9cd
César Souza <cesarsouz10@gmail.com>
2024-11-11 17:16:10 -0300

    Adicionando arquivos

---
+++ js/circulo.js
@@ -0,0 +1,11 @@
+function calcularArea(){
+
+    var raio = Number(document.getElementById('raio').value)
+
+    if (raio > 0 ){
+        var area = Math.PI * Math.pow(raio, 2);
+        document.getElementById('resultado').innerText = `A Área é: ${area.toFixed(2)}`
+    }else{
+        document.getElementById('resultado').innerText = "Por favor insira um numero valido"
+    }
+}
